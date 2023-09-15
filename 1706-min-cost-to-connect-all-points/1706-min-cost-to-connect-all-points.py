de706a5cda3a87dc7f42892d98102e136ada84d6
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-14 21:47:44 -0700

    Time: 1060 ms (81.29%), Space: 17 MB (95.36%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -4,19 +4,19 @@ class Solution:
         dist = [(0, 0)] + [(+inf, i) for i in range(1, len(points))]
         cost = 0
         while dist:
-            u_key, u_idx = heappop(dist)
+            u_dist, u_idx = heappop(dist)
             mst.add(u_idx)
-            cost += u_key
+            cost += u_dist
             next_dist = []
             while dist:
-                v_key, v_idx = heappop(dist)
+                v_dist, v_idx = heappop(dist)
                 if v_idx in mst: continue
-                w = Solution.manhattan(points[u_idx], points[v_idx])
-                heappush(next_dist, (min(w, v_key), v_idx))
+                w = Solution.manhattan_dist(points[u_idx], points[v_idx])
+                heappush(next_dist, (min(w, v_dist), v_idx))
             dist = next_dist
         return cost
 
     @staticmethod
-    def manhattan(a, b):
+    def manhattan_dist(a, b):
         return abs(a[0] - b[0]) + abs(a[1] - b[1])
         
