088a8778c588a5bb2e6cc4a25d29b54951a86d91
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-17 10:09:10 -0700

    Time: 105 ms (99.14%), Space: 16.5 MB (77.24%) - LeetHub

--- 2524-largest-positive-integer-that-exists-with-its-negative/2524-largest-positive-integer-that-exists-with-its-negative.py
+++ 2524-largest-positive-integer-that-exists-with-its-negative/2524-largest-positive-integer-that-exists-with-its-negative.py
@@ -3,9 +3,9 @@ class Solution:
         nums.sort()
         l, r = 0, len(nums) - 1
         while l < r and nums[l] < 0 and nums[r] > 0:
-            if abs(nums[l]) < nums[r]:
+            if -nums[l] < nums[r]:
                 r -= 1
-            elif abs(nums[l]) > nums[r]:
+            elif -nums[l] > nums[r]:
                 l += 1
             else:
                 return nums[r]
