121748ee06a4acf0fa6f090d11079e295be590f3
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-16 20:09:15 -0700

    Time: 683 ms (16.92%), Space: 24 MB (26.46%) - LeetHub

--- 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.py
+++ 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.py
@@ -1,22 +1,21 @@
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
         n = len(graph)
-        
-        cache = dict()
+        cache = [dict() for _ in range(n)]
+
         def dfs(node: int, mask: int) -> int:
             if mask ^ (1 << node) == (1 << n) - 1:
                 return 0
-            if (node, mask) in cache:
-                return cache[(node, mask)]
-            cache[(node, mask)] = +inf
-            
-            cache[(node, mask)] = reduce(
+            if mask in cache[node]:
+                return cache[node][mask]
+            cache[node][mask] = +inf
+            cache[node][mask] = reduce(
                 lambda acc, v: min(
                     acc, 
                     1 + dfs(v, mask ^ (1 << node)), 
                     1 + dfs(v, mask)), 
                 [v for v in graph[node] if not mask & (1 << v)],
                 +inf)
-            return cache[(node, mask)]
+            return cache[node][mask]
 
         return min(dfs(i, 0) for i in range(n))
