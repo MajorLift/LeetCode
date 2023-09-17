0bcaca4a8b391ae0985ce09670c65269f8370ef9
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-17 11:40:24 -0700

    Time: 37 ms (70.95%), Space: 16.3 MB (33.02%) - LeetHub

--- 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.py
+++ 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.py
@@ -1,5 +1,5 @@
 class Solution:
     def equalFrequency(self, word: str) -> bool:
         cnt = Counter(Counter(word).values())
-        return len(cnt) == 2 and (cnt[1] == 1 or cnt[min(cnt) + 1] == 1) \
-            or len(cnt) == 1 and (1 in cnt or 1 in cnt.values())
+        return len(cnt) == 1 and 1 in (min(cnt), cnt[min(cnt)]) \
+            or len(cnt) == 2 and 1 in (cnt[1], cnt[min(cnt) + 1])
