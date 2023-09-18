f930e4dc5130061b7ea17766a8727f1422ac7f2a
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-17 19:21:21 -0700

    Time: 236 ms (37.5%), Space: 21.7 MB (34.69%) - LeetHub

--- 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.py
+++ 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.py
@@ -1,19 +1,13 @@
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
         n = len(graph)
-        queue = deque([(i, 1 << i) for i in range(n)])
-        visited = set(queue)
-
-        steps = 0
-        while queue:
-            next_queue = deque()
-            while queue:
-                u, mask = queue.popleft()
-                if mask == (1 << n) - 1:
-                    return steps
-                for v in graph[u]:
-                    if (v, mask | (1 << v)) not in visited:
-                        visited.add((v, mask | (1 << v)))
-                        next_queue.append((v, mask | (1 << v)))
-            queue = next_queue
-            steps += 1
+        pq = [(0, i, 1 << i) for i in range(n)]
+        visited = set(pq)
+        while pq:
+            steps, u, mask = heappop(pq)
+            if mask == (1 << n) - 1:
+                return steps
+            for v in graph[u]:
+                if (v, mask | (1 << v)) not in visited:
+                    visited.add((v, mask | (1 << v)))
+                    pq.append((steps + 1, v, mask | (1 << v)))
