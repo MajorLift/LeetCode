08350f586643d3a83a6c53766e8b9546f21ad30c
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-23 15:11:42 -0700

    Time: 165 ms (53.48%), Space: 43 MB (5.01%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.py
+++ 1129-longest-string-chain/1129-longest-string-chain.py
@@ -1,9 +1,11 @@
 class Solution:
     def longestStrChain(self, words) -> int:
         words.sort(key=len)
+        word_set = set(words)
         @cache
         def dp(word):
-            if word not in words:
+            if word not in word_set:
                 return 0
-            return 1 + max(dp(word[:i] + word[i + 1:]) for i in range(len(word)))
+            return 1 + max(dp(word[:i] + word[i + 1:]) 
+                            for i in range(len(word)))
         return max(dp(word) for word in words)
