467a2dbff62597896b676176bff649848f76b4cc
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-09-23 15:05:21 -0700

    Time: 154 ms (57.62%), Space: 18.2 MB (25.62%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.py
+++ 1129-longest-string-chain/1129-longest-string-chain.py
@@ -3,6 +3,7 @@ class Solution:
         words.sort(key=len)
         memo = defaultdict(int)
         for word in words:
-            memo[word] = 1 + max(memo[word[:i] + word[i + 1:]]
-                                    for i in range(len(word)))
+            memo[word] = 1 + max(
+                memo[word[:i] + word[i + 1:]]
+                for i in range(len(word)))
         return max(memo.values())
