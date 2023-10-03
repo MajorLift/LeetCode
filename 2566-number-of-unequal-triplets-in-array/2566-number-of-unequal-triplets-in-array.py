791f9344eba09c417421f4eb8a1ac12654f72710
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-10-02 17:59:45 -0700

    Time: 1525 ms (5.01%), Space: 16.2 MB (85.14%) - LeetHub

--- 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.py
+++ 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.py
@@ -1,5 +1,5 @@
 class Solution:
     def unequalTriplets(self, nums: List[int]) -> int:
         return sum(1 
-            for i, j, k in combinations(nums, 3) 
-            if i != j and j != k and k != i)
+            for triplet in combinations(nums, 3) 
+            if all(l != r for l, r in combinations(triplet, 2)))
