1c8f2470316ceb0772bddb67e5acd9ce92a4f1ca
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-10-15 14:07:28 -0700

    Time: 3013 ms (5.13%), Space: 30 MB (78.46%) - LeetHub

--- 1705-count-unhappy-friends/1705-count-unhappy-friends.py
+++ 1705-count-unhappy-friends/1705-count-unhappy-friends.py
@@ -1,14 +1,21 @@
 class Solution:
     def unhappyFriends(self, n: int, preferences: List[List[int]], pairs: List[List[int]]) -> int:
-        pairOf = {**{x: y for x, y in pairs}, **{y: x for x, y in pairs}}
-        return sum(
-            any(x in Person(preferences[u]).getPreferredThan(pairOf[u]) 
-                for u in Person(preferences[x]).getPreferredThan(pairOf[x])) 
-            for x in range(n))
-
+        unhappy = set()
+        for (x, y), (u, v) in combinations(pairs, 2):
+            X, Y, U, V = (Person(preferences[p]) for p in (x, y, u, v))
+            if X.prefersThisOverThat(u, y) and U.prefersThisOverThat(x, v):
+                unhappy |= set([x, u])
+            if X.prefersThisOverThat(v, y) and V.prefersThisOverThat(x, u):
+                unhappy |= set([x, v])
+            if Y.prefersThisOverThat(u, x) and U.prefersThisOverThat(y, v):
+                unhappy |= set([y, u])
+            if Y.prefersThisOverThat(v, x) and V.prefersThisOverThat(y, u):
+                unhappy |= set([y, v])
+        return len(unhappy)
+            
 class Person:
     def __init__(self, preference):
-        self.preference = preference
+        self.rank = {e: i for i, e in enumerate(preference)}
     
-    def getPreferredThan(self, this):
-        return set(self.preference[:self.preference.index(this)])
+    def prefersThisOverThat(self, this, that):
+        return self.rank[this] < self.rank[that]
