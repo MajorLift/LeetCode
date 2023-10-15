8a7e8e71ea566a7dbe1ef13de1051d5cd1bfadfb
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-10-15 14:06:42 -0700

    Time: 341 ms (80%), Space: 29.9 MB (78.46%) - LeetHub

--- 1705-count-unhappy-friends/1705-count-unhappy-friends.py
+++ 1705-count-unhappy-friends/1705-count-unhappy-friends.py
@@ -1,14 +1,10 @@
 class Solution:
     def unhappyFriends(self, n: int, preferences: List[List[int]], pairs: List[List[int]]) -> int:
-        people = {i: Person(e) for i, e in enumerate(preferences)}
-        pair = {**{x: y for x, y in pairs}, **{y: x for x, y in pairs}}
-        ans = 0
-        for x in range(n):
-            for u in people[x].getPreferredThan(pair[x]):
-                if x in people[u].getPreferredThan(pair[u]):
-                    ans += 1
-                    break
-        return ans
+        pairOf = {**{x: y for x, y in pairs}, **{y: x for x, y in pairs}}
+        return sum(
+            any(x in Person(preferences[u]).getPreferredThan(pairOf[u]) 
+                for u in Person(preferences[x]).getPreferredThan(pairOf[x])) 
+            for x in range(n))
 
 class Person:
     def __init__(self, preference):
