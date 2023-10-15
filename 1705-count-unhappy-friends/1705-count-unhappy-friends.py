01dfb84cb05fbf26da9d9cbe83d1c43fdee90edc
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-10-15 16:19:38 -0700

    Time: 365 ms (31.54%), Space: 29.9 MB (91.79%) - LeetHub

--- 1705-count-unhappy-friends/1705-count-unhappy-friends.py
+++ 1705-count-unhappy-friends/1705-count-unhappy-friends.py
@@ -1,12 +1,12 @@
 class Solution:
     def unhappyFriends(self, n: int, preferences: List[List[int]], pairs: List[List[int]]) -> int:
-        pairOf = {**{x: y for x, y in pairs}, **{y: x for x, y in pairs}}
+        partnerOf = {**{x: y for x, y in pairs}, **{y: x for x, y in pairs}}
         return sum(
             any(x in U.getPreferredOver(v) 
                 for u in X.getPreferredOver(y)
-                    if (U := Person(preferences[u]), v := pairOf[u]))
+                    if (U := Person(preferences[u]), v := partnerOf[u]))
             for x in range(n)
-                    if (X := Person(preferences[x]), y := pairOf[x]))
+                    if (X := Person(preferences[x]), y := partnerOf[x]))
 
 class Person:
     def __init__(self, preference):
